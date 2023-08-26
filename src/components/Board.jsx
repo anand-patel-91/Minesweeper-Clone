79d7c8fb866b1716edcb54c4d4bc1ea7ffeab1e6
anand-patel-91 <patel91a@gmail.com>
2023-08-26 14:45:05 +0530

    fixed bomb on first click bug

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import CreateBoard from '../utils/CreateBoard'
 import Cell from './Cell';
 import reveale from '../utils/reveale';
@@ -8,6 +8,11 @@ const Board = () => {
     let BoardSize = 10;
     let mines = 20;
 
+    const firstClick = useRef({
+        status: true,
+        row: -1,
+        col: -1
+    })
     const [grid, setGrid] = useState([])
     const [safe, setSafe] = useState(0)
     const [mineLocation, setMineLocation] = useState([])
@@ -19,11 +24,22 @@ const Board = () => {
         setMineLocation(newBoard.mineLocation)
         setSafe(BoardSize * BoardSize - mines)
         setGameOver(false)
+        firstClick.current = {
+            status: true,
+            row: -1,
+            col: -1
+        }
     }
 
     useEffect(() => {
-        return freshBoard();
-    }, []);
+        freshBoard()
+    }, [])
+
+    useEffect(() => {
+        if (firstClick.current.row != -1 && firstClick.current.col != -1) {
+            revealCell(firstClick.current.row, firstClick.current.col)
+        }
+    }, [firstClick.current])
 
     const updateFlag = (e, x, y) => {
         e.preventDefault()
@@ -36,17 +52,39 @@ const Board = () => {
     }
 
     const revealCell = (x, y) => {
-        if (gameOver || !safe) {
+        console.log(grid);
+        if (gameOver || !safe || grid[x][y].revealed) {
             return;
         }
+
         let newGrid = JSON.parse(JSON.stringify(grid))
-        if (newGrid[x][y].value === -1) {
+        if (firstClick.current.status === true && newGrid[x][y].value != 0) {
+            let newBoard = CreateBoard(BoardSize, mines)
+            while (newBoard.board[x][y].value != 0) {
+                newBoard = CreateBoard(BoardSize, mines)
+            }
+            setGrid(newBoard.board);
+            setMineLocation(newBoard.mineLocation)
+
+            firstClick.current = {
+                status: false,
+                row: x,
+                col: y
+            }
+        }
+
+        else if (newGrid[x][y].value === -1) {
             alert("Game Over")
             setGameOver(true)
             for (let i = 0; i < mineLocation.length; i++) {
                 newGrid[mineLocation[i][0]][mineLocation[i][1]].revealed = true
             }
             setGrid(newGrid)
+            firstClick.current = {
+                status: false,
+                row: -1,
+                col: -1
+            }
         }
         else {
             let revealedBoard = reveale(newGrid, x, y, safe)
@@ -55,6 +93,11 @@ const Board = () => {
             if (!revealedBoard.newSafe) {
                 setGameOver(true)
                 alert("Congrats you win!")
+            }
+            firstClick.current = {
+                status: false,
+                row: -1,
+                col: -1
             }
         }
     }
